8f3e9ee4dd13cc5eac06af439ad9790abcdcc70d
Maria Davis <mdavis81@my.bcit.ca>
2020-04-24 12:42:09 -0700

    cleaned up code, added comments

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.listen('3000', () => {
   console.log('Server started on port 3000');
 });
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -23,25 +23,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
 module.exports = app;
 
-// database connection
+// Database connection
 var mysql = require('mysql');
 const db = mysql.createConnection({
   host: 'localhost',
@@ -55,8 +39,39 @@ db.connect((err) => {
   console.log('Connected!');
 });
 
-router = express.Router();
+// Creates database trackr
+app.get('/createDatabase', function(req, res) {
+  let sql = "CREATE DATABASE IF NOT EXISTS trackr";
+  db.query(sql, function(err) {
+    if(err) throw err;
+    console.log("Database created");
+  });
+});
 
+// Creates table expenses
+app.get('/createTable', function(req, res) {
+  db.query('CREATE TABLE IF NOT EXISTS expenses('
+    + 'name VARCHAR(255) NOT NULL,'
+    + 'cost DOUBLE NOT NULL,'
+    + 'category ENUM(\'Groceries\', \'Clothing\', \'Entertainment\'', 
+    + '\'Housing\', \'Other\') NOT NULL,'
+    +  ')', function (err) {
+      if (err) throw err;
+      console.log("Table created");
+    });
+});
+
+// Submits new expense to db
+app.post('/expenseSubmitted', function(req, res) {
+  var sql = `INSERT INTO expenses (name, cost, category) 
+  VALUES ('${req.body.name}', ${req.body.cost}, '${req.body.category}')`;
+  db.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Expense added");
+  });
+});
+
+// Renders page displaying list of expenses
 app.get('/expenses', function(req, res, next) {
   var sql = 'SELECT * FROM expenses';
   db.query(sql, function (err, data, fields) {
@@ -66,40 +81,13 @@ app.get('/expenses', function(req, res, next) {
   });
 });
 
-module.exports = router;
-
-app.get('/', function(req, res) {
-  console.log("First get");
-});
-
-app.get('/createDatabase', function(req, res) {
-  let sql = "CREATE DATABASE IF NOT EXISTS trackr";
-  db.query(sql, function(err) {
-    if(err) throw err;
-    console.log("Database created");
-  });
-});
-
-app.get('/createTable', function(req, res) {
-  db.query('CREATE TABLE IF NOT EXISTS expenses('
-    + 'name VARCHAR(255) NOT NULL,'
-    + 'cost DOUBLE NOT NULL,'
-    + 'category ENUM(\'Groceries\', \'Clothing\', \'Entertainment\', \'Housing\', \'Other\') NOT NULL,'
-    +  ')', function (err) {
-      if (err) throw err;
-      console.log("Table created");
-    });
-});
-
-app.post('/expenseSubmitted', function(req, res) {
-  var sql = `INSERT INTO expenses (name, cost, category) VALUES ('${req.body.name}', ${req.body.cost}, '${req.body.category}')`;
-  db.query(sql, function (err, result) {
+// Deletes row from table (under construction)
+app.post('/itemDeleted', function(req, res, next) {
+  console.log(req.params.id.name);
+  var sql = `DELETE FROM expenses WHERE name = '${req.params.item.name}'`;
+  db.query(sql, function (err, data, fields) {
     if (err) throw err;
-    console.log("Expense added");
+    res.render('expense-list', {title: 'User List', userData: data});
+    console.log("Item deleted");
   });
 });
-
-// exports.expenseSubmitted = function (req, res) {
-//   console.log(req.body);
-//   res.render('expenseSubmitted', req.body);
-// };
